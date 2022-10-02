e9ca2af252e38bdd4462f5a9dff466cb0f240906
Maurilio Reis <mauriliossreis@gmail.com>
2022-10-02 13:32:21 -0300

    Added assest / Created objects in scene / Scripts
    *************************************************************** Added assest ▼
    
    --------- Sprites ▼
    
    --------------- Sample ▼
    ----------------------- Added Sprite: project draft
    
    --------------- Ships ▼
    ----------------------- Renamed sprites
    
    *************************************************************** Created objects in scene ▼
    
    --------- Created an Edited Player ▼
    --------------- Added components ▼
    ----------------------- RigidBody 2D
    ----------------------- Capsule Collider 2D
    
    --------- Created an Edited Enemy
    --------------- Added components ▼
    ----------------------- RigidBody 2D
    ----------------------- Capsule Collider 2D
    
    --------- EventSystem ▼
    --------------- Added component script: TypeShips
    
    *************************************************************** Scripts ▼
    
    --------- Folder: Scripts ▼
    --------------- Created Folder: EventSystem ▼
    ----------------------- Created Script: TypeShips

---
+++ Project MKS/Assets/Scripts/EventSystem/TypeShips.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TypeShips : MonoBehaviour
+{
+    public Sprite[] shipWhite;
+    public Sprite[] shipBlack;
+    public Sprite[] shipRed;
+    public Sprite[] shipGreen;
+    public Sprite[] shipBlue;
+    public Sprite[] shipYellow;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
